9a8e0f5cfb53772b407cca0c9ecbc343fd2ba975
Su3h7aM <matheusgasource@gmail.com>
2024-04-16 20:19:13 -0300

    Improved consistency in the use of variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,20 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    
+    #set ZELLIJ_AUTO_ATTACH true
+    #set ZELLIJ_AUTO_EXIT false
     #eval "$(zellij setup --generate-auto-start fish)"
 end
 
 set -g fish_greeting
 
+if not set -q ZELLIJ
+    zellij attach default -c
+end
+
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'micromamba shell init' !!
-set -gx MAMBA_EXE "/home/matheus/.local/bin/micromamba"
+set -gx MAMBA_EXE "$HOME/.local/bin/micromamba"
 set -gx MAMBARC "$XDG_CONFIG_HOME/mamba/mambarc.yml"
 set -gx MAMBA_ROOT_PREFIX "$XDG_CONFIG_HOME/mamba"
 $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
